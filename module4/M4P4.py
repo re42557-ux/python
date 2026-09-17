658c670eff2bb07bcbbf0d9d13c56fcc662b11a1
re42557-ux <re42557@mail.harpercollege.edu>
2026-09-17 13:19:26 -0500

    M4P4.py

---
+++ module4/M4P4.py
@@ -0,0 +1,7 @@
+first_name = input("enter first name: ")
+steps = int(input("enter number of steps walked: "))
+
+calories_burned = steps * (0.25)
+
+print("First name:",first_name)
+print("Calories burned:",calories_burned)
